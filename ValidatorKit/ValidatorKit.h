eeba2480af29700265b68b727f68b8f897ed3b30
Tales Conrado <talesmateuscc@gmail.com>
2021-07-22 10:33:15 -0300

    Initial Commit

---
+++ ValidatorKit/ValidatorKit.h
@@ -0,0 +1,18 @@
+//
+//  ValidatorKit.h
+//  ValidatorKit
+//
+//  Created by Tales Conrado on 22/07/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ValidatorKit.
+FOUNDATION_EXPORT double ValidatorKitVersionNumber;
+
+//! Project version string for ValidatorKit.
+FOUNDATION_EXPORT const unsigned char ValidatorKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ValidatorKit/PublicHeader.h>
+
+
