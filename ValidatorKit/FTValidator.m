756cc3a6849fba52aa5447e94782bdf15cc6b92a
Fernando de Lucas <54122934+FernandoDLucas@users.noreply.github.com>
2021-07-22 11:27:49 -0300

    Created enum of Errors and add test in Function Max Length

--- ValidatorKit/FTValidator.m
+++ ValidatorKit/FTValidator.m
@@ -4,8 +4,9 @@
 //
 //  Created by Tales Conrado on 22/07/21.
 //
-
+#import <Foundation/Foundation.h>
 #import "FTValidator.h"
+#import "ErrorHandle.h"
 
 @implementation FTValidator
 @synthesize maxLength, minLength, allowSpecialCharacters;
@@ -21,8 +22,12 @@
 
 - (BOOL) validate:(NSString *) text {
     if (maxLength > 0) {
-        if (![self validateMaxSize:text andSize:maxLength]) {
-            return NO;
+        @try{
+            [self validateMaxSize:text andSize:maxLength];
+        } @catch(ErrorHandle *e) {
+            NSLog(@"Exception: %@", e.errorMessage);
+        } @finally{
+            NSLog(@"Continuando");
         }
     }
 
@@ -42,12 +47,17 @@
 }
 
 
-- (BOOL) validateMaxSize:(NSString *) text andSize: (int) size {
+- (void) validateMaxSize:(NSString *) text andSize: (int) size {
     if ([text length] > size) {
-        return NO;
+//        @throw [NSException exceptionWithName: (@"Error") reason:(@"Error") userInfo:nil];
+        ErrorHandle *error = [[ErrorHandle alloc] init];
+        error.errorMessage = [error errorFeedback:tooLong];
+        @throw error;
+//        return NO;
     } else {
-        return YES;
+//        return YES;
     }
+    
 }
 
 - (BOOL) validateMinSize:(NSString *) text andSize: (int) size {
@@ -73,3 +83,13 @@
 @end
 
 
+@implementation ErrorHandle
+
+-(NSString *) errorFeedback: (InvalidString)invalidated{
+    switch (invalidated) {
+        case tooShort: return(@"Invalid"); break;
+        case tooLong: return(@"Mais caracteres do que esperado"); break;
+        case specialCharacterFound: return(@"Invalid"); break;
+    }
+}
+@end
